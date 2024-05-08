d47f3372c6ba1a9abb5bfdf2e9cb1566f94655cb
Mory DIA <dia.mory@heb.com>
2024-05-08 16:40:07 -0500

    Wrap Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,35 @@ import { faker } from "@faker-js/faker";
 })
 export class AppComponent {
 
-  random_text = faker.lorem.sentence({min:3, max:5});
-  entredText = '';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2023
+  };
 
+  onNameChange(event:any){
+    this.name = event.target.value;
+  }
 
-  onInput(event:any){
-    this.entredText = event.target.value;
+  onDateChange(event:any){
+    this.date = event.target.value;
+  }
+
+  onAmountChange(even:any){
+    this.amount = parseFloat(even.target.value);
+  }
+
+  onHeightChange(event:any){
+    this.height = event.target.value;
+  }
+
+  onMileChange(event:any){
+    this.miles = event.target.value;
   }
 
 
