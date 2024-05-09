ac14e9014b7a4b4e11e836cf7e9f162613d6f6c8
Mory DIA <dia.mory@heb.com>
2024-05-08 22:47:01 -0500

    custom structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClassNameDirective } from './class-name.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClassNameDirective
+    ClassNameDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
