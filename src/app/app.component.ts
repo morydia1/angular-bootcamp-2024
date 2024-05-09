9fc4cf5a0ae670d3e54ef33a98ce78565a422703
Mory DIA <dia.mory@heb.com>
2024-05-08 23:23:58 -0500

    card merged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,32 @@ import { faker } from "@faker-js/faker";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature',
+      content: 'I saw this neat today'
+    },
+    {
+      title: 'Mory Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'Mory',
+      content: 'I saw you biking today'
+    },
+    {
+      title: 'Mory Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'Mory',
+      content: 'I saw you biking today'
+    }
+  ]  
 
 }
